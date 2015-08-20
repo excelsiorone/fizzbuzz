d29a976566eae9bf82acc749b250aec87a54e54f
Ozzy <ozzytriff@gmail.com>
2015-08-20 14:43:27 -0400

    fizzbuzz challenge

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$("form").on("submit", function(e) {
+		e.preventDefault();
+	})
+
+	for (var i = 1; i<=100; i++) {
+
+		if (i % 3 === 0 && i % 5 === 0) {
+			$("body").append("<li>" +"FizzBuzz"+ "</li>");
+		}
+
+		else if (i % 3 === 0) {
+			$("body").append("<li>" +"fizz"+ "</li>");
+	}
+
+		else if (i % 5 === 0) {
+			$("body").append("<li>" +"buzz"+ "</li>");
+	}
+		else {
+			$("body").append(i);
+		}
+
+	}
+
+	})
+
+	
+
